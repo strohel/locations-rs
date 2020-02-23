a2451055d24cd2b4e789fac059a496a819db7179
Matěj Laitl <matej@laitl.cz>
2020-02-23 13:13:26 +0100

    Enable some more rustc warnings
    The idea is to make most out of the compiler.
    
    See https://doc.rust-lang.org/rustc/lints/listing/allowed-by-default.html

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 // Make writing "unsafe" in code a compilation error. We should not need unsafe at all.
 #![forbid(unsafe_code)]
+// Warn on generally recommended lints that are not enabled by default.
+#![warn(future_incompatible, rust_2018_idioms, unused, macro_use_extern_crate)]
+// Warn when we write more code than necessary.
+#![warn(unused_lifetimes, single_use_lifetimes, unreachable_pub, trivial_casts)]
+// Warn when we don't implement (derive) commonly needed traits. May be too strict.
+#![warn(missing_copy_implementations, missing_debug_implementations)]
 // Turn on some extra Clippy (Rust code linter) warnings. Run `cargo clippy`.
 #![warn(clippy::all, clippy::nursery)]
 
