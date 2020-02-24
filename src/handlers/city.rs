77d6b356554a47641986ae1edb76ad315385cb66
Matěj Laitl <matej@laitl.cz>
2020-02-24 01:04:41 +0100

    WIP: Plug in Elasticsearch and call it from the endpoint

--- src/handlers/city.rs
+++ src/handlers/city.rs
@@ -1,7 +1,11 @@
 //! Handlers for /city/* endpoints.
 
 use crate::{handlers::IntoOkResponse, Request};
+use elasticsearch::GetParts;
+use log::info;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use tide::ResultExt;
 
 /// Query for the `/city/v1/get` endpoint.
 #[derive(Deserialize)]
@@ -24,6 +28,22 @@ struct CityResponse {
 /// The `/city/v1/get` endpoint. HTTP input: [CityQuery], output: [CityResponse].
 pub(crate) async fn get(req: Request) -> tide::Result {
     let query: CityQuery = req.query()?;
+
+    let es = &req.state().elasticsearch;
+    let es_response =
+        es.get(GetParts::IndexId("city", &query.id.to_string())).send().await.server_err()?;
+
+    let es_resp_code = es_response.status_code().as_u16();
+    info!("Elasticsearch response status: {}.", es_resp_code);
+    if es_resp_code == 404 {
+        return Ok(tide::Response::new(404).body_string(format!("City#{} not found.\n", query.id)));
+    }
+    if es_resp_code != 200 {
+        return Ok(tide::Response::new(500).body_string(format!("ES response {}.\n", es_resp_code)));
+    }
+
+    let es_resp_body = es_response.read_body::<Value>().await.server_err()?;
+    info!("Elasticsearch response body: {:}.", es_resp_body);
 
     let city = CityResponse {
         country_iso: "CZ".to_string(),
--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,15 @@
 // Turn on some extra Clippy (Rust code linter) warnings. Run `cargo clippy`.
 #![warn(clippy::all, clippy::nursery)]
 
+use elasticsearch::{http::transport::Transport, Elasticsearch};
 use env_logger::DEFAULT_FILTER_ENV;
+use log::info;
 use std::{env, io};
 
 mod handlers;
 
 /// Convenience type alias to be used by handlers.
-type Request = tide::Request<()>;
+type Request = tide::Request<AppState>;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -27,7 +29,7 @@ async fn main() -> io::Result<()> {
     }
     pretty_env_logger::init();
 
-    let mut app = tide::new();
+    let mut app = tide::with_state(AppState::new().await);
     app.middleware(tide::middleware::RequestLogger::new());
 
     app.at("/city/v1/get").get(handlers::city::get);
@@ -36,4 +38,26 @@ async fn main() -> io::Result<()> {
     app.at("/*").all(handlers::fallback::not_found);
 
     app.listen("127.0.0.1:8080").await
+}
+
+struct AppState {
+    elasticsearch: Elasticsearch,
+}
+
+impl AppState {
+    async fn new() -> Self {
+        let es_url = format!(
+            "http://{}:{}/",
+            env::var("GOOUT_ELASTIC_HOST").unwrap(),
+            env::var("GOOUT_ELASTIC_PORT").unwrap()
+        );
+        let es_transport = Transport::single_node(&es_url).unwrap();
+        let elasticsearch = Elasticsearch::new(es_transport);
+
+        let es_result = elasticsearch.ping().send().await;
+        let es_resp = es_result.map_err(|e| format!("Cannot ping Elasticsearch: {}.", e)).unwrap();
+        info!("Elasticsearch ping status: {}.", es_resp.status_code());
+
+        Self { elasticsearch }
+    }
 }
