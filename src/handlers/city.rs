e953989992354a743f5a2ce67116eba865329327
Matěj Laitl <matej@laitl.cz>
2020-02-25 02:24:46 +0100

    Make /city/v1/get endpoint working
    Including language.
    
    The spagetti code can be improved a lot, this is just a quickest attempt to make
    it working.

--- src/handlers/city.rs
+++ src/handlers/city.rs
@@ -2,15 +2,15 @@
 
 use crate::{handlers::IntoOkResponse, Request};
 use elasticsearch::GetParts;
-use log::info;
+use log::{error, info};
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
 use tide::ResultExt;
 
 /// Query for the `/city/v1/get` endpoint.
 #[derive(Deserialize)]
 struct CityQuery {
     id: u64,
+    language: String, // TODO: serde can probably limit to 2 chars
 }
 
 /// All city endpoints respond with this payload (or a composition of it).
@@ -42,15 +42,55 @@ pub(crate) async fn get(req: Request) -> tide::Result {
         return Ok(tide::Response::new(500).body_string(format!("ES response {}.\n", es_resp_code)));
     }
 
-    let es_resp_body = es_response.read_body::<Value>().await.server_err()?;
-    info!("Elasticsearch response body: {:}.", es_resp_body);
+    let response_body_res = es_response.read_body::<ElasticGetResponse<ElasticCity>>().await;
+    let response_body = response_body_res
+        .map_err(|e| {
+            error!("Failed to read ES response: {}", e);
+            e
+        })
+        .server_err()?;
+    info!("Elasticsearch response body: {:?}.", response_body);
 
+    let es_city = response_body._source;
     let city = CityResponse {
-        country_iso: "CZ".to_string(),
-        id: query.id,
-        is_featured: true,
-        name: "Synthesised City".to_string(),
-        region_name: "Fake Region".to_string(),
+        country_iso: es_city.country_iso,
+        id: es_city.id,
+        is_featured: es_city.is_featured,
+        name: match query.language.as_str() {
+            "cs" => es_city.name_cs,
+            "de" => es_city.name_de,
+            "en" => es_city.name_en,
+            "pl" => es_city.name_pl,
+            "sk" => es_city.name_sk,
+            _ => es_city.name_en,
+        },
+        region_name: format!("Region#{}", es_city.region_id), // TODO
     };
     city.into_ok_response()
+}
+
+#[derive(Debug, Deserialize)]
+struct ElasticGetResponse<T> {
+    _source: T,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct ElasticCity {
+    #[serde(rename = "countryISO")]
+    country_iso: String,
+    id: u64,
+    #[serde(default)] // TODO: isFeatured is not yet in Elastic, fall-back to default of false
+    is_featured: bool,
+    #[serde(rename = "name.cs")] // TODO: serde surely has better way to do this
+    name_cs: String,
+    #[serde(rename = "name.de")]
+    name_de: String,
+    #[serde(rename = "name.en")]
+    name_en: String,
+    #[serde(rename = "name.pl")]
+    name_pl: String,
+    #[serde(rename = "name.sk")]
+    name_sk: String,
+    region_id: u64,
 }
