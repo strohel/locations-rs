20f3ee8d31fdbcdac843d9f3928efe74ec69c479
Matěj Laitl <matej@laitl.cz>
2020-02-29 01:16:05 +0100

    Use new ErrorResponse in fallback endpoint
    Greatly simplifies code.

--- src/handlers/fallback.rs
+++ src/handlers/fallback.rs
@@ -1,12 +1,7 @@
 //! Fallback handler, as [tide] provides only very rudimentary fallback (with no body text).
 
-use crate::Request;
-use http::StatusCode;
-use log::warn;
-use tide::IntoResponse;
+use crate::{response::ErrorResponse, Request};
 
-pub(crate) async fn not_found(req: Request) -> impl IntoResponse {
-    warn!("Responding HTTP 404 Not Found to '{}'.", req.uri());
-
-    format!("Resource {} does not exist.\n", req.uri()).with_status(StatusCode::NOT_FOUND)
+pub(crate) async fn not_found(req: Request) -> ErrorResponse {
+    ErrorResponse::NotFound(format!("Resource {} does not exist.", req.uri()))
 }
