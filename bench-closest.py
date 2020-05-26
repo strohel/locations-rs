efde68c2387900c8153f9effad91329a3c18bb4f
Matej Laitl <laitl@goout.cz>
2020-05-26 16:17:12 +0200

    Update bench-closest.py - create dir per query type, change queries
    Also accept number of runs

--- bench-closest.py
+++ bench-closest.py
@@ -2,24 +2,37 @@
 
 """
 Benchmark the /v1/city/closest endpoint.
+
+Usage:
+  bench-closest.py [--runs=<n>]
+
+Options:
+  --runs=<n>    Number of runs to perform [default: 2].
 """
 
 from pathlib import Path
 import subprocess
 
+from docopt import docopt
 
-def main():
+
+def main(runs):
     endpoint_paths = {
-        'polygon': '/city/v1/closest?language=pl&lat=50.107&lon=14.574',  # Praha
-        'point': '/city/v1/closest?language=cs&lat=49.84&lon=12.92',  # Kozolupy
+        'praha-by-geometry': '/city/v1/closest?language=pl&lat=50.107&lon=14.574',
+        'otice-by-geometry': '/city/v1/closest?language=pl&lat=49.915&lon=17.870',
+        'otice-by-centroid': '/city/v1/closest?language=pl&lat=49.914&lon=17.877',
     }
     images = ['1rt-rs', '2rt-kt', '1rt-kt']
-    test_image = str(Path(__file__).parent / 'test-image.py')
 
-    for i in range(1, 3):
+    script_dir = Path(__file__).parent.absolute()
+    test_image = str(script_dir / 'test-image.py')
+    render_tests = str(script_dir / 'render-tests.py')
+
+    for i in range(1, runs + 1):
         for ep_type, path in endpoint_paths.items():
+            Path(ep_type).mkdir(exist_ok=True)
             for image in images:
-                outfile = f'{ep_type}-{image}-{i}.checks.bench.json'
+                outfile = f'{ep_type}/{image}-{i}.checks.bench.json'
                 if Path(outfile).exists():
                     print(f'{outfile} exists, skipping this benchmark.')
                     continue
@@ -28,6 +41,15 @@ def main():
                     run([test_image, image, '--bench-url', path, '--bench-out', outfile], check=True)
                 except subprocess.CalledProcessError:
                     print('Called process exited with non-zero exit status, but continuing.')
+
+    html_lines = []
+    for ep_type in endpoint_paths:
+        run([render_tests], cwd=ep_type)
+        html_lines.append(f'<a href="{ep_type}/bench-results.html">{ep_type}</a>')
+
+    html = '<br>\n'.join(html_lines)
+    with open('index.html', 'w') as f:
+        f.write(html)
 
 
 # TODO: deduplicate
@@ -44,4 +66,5 @@ def run(program_args, **kwargs):
 
 
 if __name__ == '__main__':
-    main()
+    args = docopt(__doc__)
+    main(int(args['--runs']))
